7c390f8b5e80a1efb894519b4684a88927e6d4e0
bmarkons <mamaveb@gmail.com>
2017-06-14 15:06:12 +0200

    Places CRUD

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -1,2 +1,66 @@
 class Admin::PlacesController < ApplicationController
+  before_action :set_place, only: [:show, :edit, :update, :destroy]
+  before_action :set_regions, only: [:edit, :update, :new, :create]
+
+  def index
+    @places = Place.all
+  end
+
+  def show
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def edit
+  end
+
+  def create
+    @place = Place.new(place_params)
+
+    respond_to do |format|
+      if @place.save
+        format.html { redirect_to @place, notice: 'Place was successfully created.' }
+        format.json { render :show, status: :created, location: @place }
+      else
+        format.html { render :new }
+        format.json { render json: @place.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @place.update(place_params)
+        format.html { redirect_to @place, notice: 'Place was successfully updated.' }
+        format.json { render :show, status: :ok, location: @place }
+      else
+        format.html { render :edit }
+        format.json { render json: @place.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @place.destroy
+    respond_to do |format|
+      format.html { redirect_to places_url, notice: 'Place was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  def set_regions
+    @regions = Region.all
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :region_id)
+  end
 end
