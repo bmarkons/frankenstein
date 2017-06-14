38354f18ecc90fb1d7c37b41814fa97b5f6647ec
bmarkons <mamaveb@gmail.com>
2017-06-14 15:15:33 +0200

    Accomodation types CRUD

--- app/controllers/admin/accommodation_types_controller.rb
+++ app/controllers/admin/accommodation_types_controller.rb
@@ -1,2 +1,61 @@
 class Admin::AccommodationTypesController < ApplicationController
+  before_action :set_accommodation_type, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @accommodation_types = AccommodationType.all
+  end
+
+  def show
+  end
+
+  def new
+    @accommodation_type = AccommodationType.new
+  end
+
+  def edit
+  end
+
+  def create
+    @accommodation_type = AccommodationType.new(accommodation_type_params)
+
+    respond_to do |format|
+      if @accommodation_type.save
+        format.html { redirect_to @accommodation_type, notice: 'Accommodation type was successfully created.' }
+        format.json { render :show, status: :created, location: @accommodation_type }
+      else
+        format.html { render :new }
+        format.json { render json: @accommodation_type.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @accommodation_type.update(accommodation_type_params)
+        format.html { redirect_to @accommodation_type, notice: 'Accommodation type was successfully updated.' }
+        format.json { render :show, status: :ok, location: @accommodation_type }
+      else
+        format.html { render :edit }
+        format.json { render json: @accommodation_type.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @accommodation_type.destroy
+    respond_to do |format|
+      format.html { redirect_to accommodation_types_url, notice: 'Accommodation type was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_accommodation_type
+    @accommodation_type = AccommodationType.find(params[:id])
+  end
+
+  def accommodation_type_params
+    params.require(:accommodation_type).permit(:name)
+  end
 end
--- test/controllers/admin/accommodation_types_controller_test.rb
+++ test/controllers/admin/accommodation_types_controller_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class Admin::AccommodationTypesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @accommodation_type = accommodation_types(:one)
+  end
+
+  test "should get index" do
+    get accommodation_types_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_accommodation_type_url
+    assert_response :success
+  end
+
+  test "should create accommodation_type" do
+    assert_difference('Admin::AccommodationType.count') do
+      post accommodation_types_url, params: { accommodation_type: {  } }
+    end
+
+    assert_redirected_to accommodation_type_url(Admin::AccommodationType.last)
+  end
+
+  test "should show accommodation_type" do
+    get accommodation_type_url(@accommodation_type)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_accommodation_type_url(@accommodation_type)
+    assert_response :success
+  end
+
+  test "should update accommodation_type" do
+    patch accommodation_type_url(@accommodation_type), params: { accommodation_type: {  } }
+    assert_redirected_to accommodation_type_url(@accommodation_type)
+  end
+
+  test "should destroy accommodation_type" do
+    assert_difference('Admin::AccommodationType.count', -1) do
+      delete accommodation_type_url(@accommodation_type)
+    end
+
+    assert_redirected_to accommodation_types_url
+  end
 end
